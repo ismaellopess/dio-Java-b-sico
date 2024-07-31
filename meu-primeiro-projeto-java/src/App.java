471d6afd79068135b3118c1886c2d272d6f697c2
ismaellopess <lopesismael688@gmail.com>
2024-07-30 21:24:05 -0300

    ALTERAÇAO DO NOME

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("ISMAEL LOIPES aluno da turma dio 30/07/2024 ás 21:20 hrsS");
+        System.out.println("ISMAEL LOPES aluno da turma dio 30/07/2024 ás 21:20 hrsS");
     }
 }
